c39be05c6c1c24a7a23ad9c09b386a911dd56f2c
SandyRuben <flier12345@gmail.com>
2025-03-21 14:18:17 -0700

    first commit

--- sketch.js
+++ sketch.js
@@ -1,24 +1,91 @@
 function setup() {
-  createCanvas(400, 400, WEBGL);
+  createCanvas(400, 400);
+  silhouetteCanvas = createGraphics(width, height);
+  waning = true;
+  waxing = false;
 
-  
-  
+  celebrating = false;
+  noStroke();
+  rightWidth = width *.17;
+  leftWidth = width *.17;
+
+  celebrationSound = loadSound("assets/celebrateSound.mp3");
+
+  confetti = loadImage("assets/Confetti.jpg");
 }
 
 function draw() {
-  background(220);
-
-  push();
-  translate(0, 0, -100);
-  rotateWithFrameCount();
-  sphere(50);
-  pop();
+  silhouette();
+  // if (!celebrating) {
+  //   silhouette();
+  // } else {
+  //   celebrate();
+  //   if (this.celebrating && !this.celebrationSound.isPlaying()) {
+  //     this.celebrating = false;
+  //   } 
+  // }
+  
 }
 
-// Rotate 1 degree per frame along all three axes
-//Source - https://p5js.org/examples/3d-geometries/
-function rotateWithFrameCount() {
-  rotateZ(frameCount);
-  rotateX(frameCount);
-  rotateY(frameCount);
+function silhouette() {
+  background(220);
+
+  //moon ellipse change to image later
+  ellipse(width/2, height/2, 200, 200);
+
+  //moon silhouette
+
+  //logic for waxing or waning
+
+  //limits for widths
+  // rightwidth = width *.83;
+  // leftWidth = width *.17;
+  print(this.celebrationSound.isLoaded());
+
+  if (waning) {
+    leftWidth++;
+  }
+  
+  if (leftWidth >= width *.83) {
+    waning = false;
+    waxing = true;
+  }
+
+  if (waxing) {
+    rightWidth++;
+  }
+
+  if (rightWidth >= width*.83) {
+    waxing = false;
+  }
+
+  if (!waning && !waxing) {
+    rightWidth = width *.17;
+    leftWidth = width *.17;
+    waning = true;
+    celebrate();
+  }
+
+  
+  push();
+  silhouetteCanvas.noStroke();
+  silhouetteCanvas.clear();
+  silhouetteCanvas.filter(BLUR);
+  silhouetteCanvas.fill(0, 0, 0, 200);
+  silhouetteCanvas.beginShape();
+  silhouetteCanvas.vertex(width*.5, height*.25);
+  silhouetteCanvas.bezierVertex(rightWidth, height*.25, rightWidth, height*.75, width*.5, height*.75);
+  silhouetteCanvas.bezierVertex(leftWidth, height*.75, leftWidth, height*.25, width*.5, height*.25);
+  silhouetteCanvas.endShape();
+  image(silhouetteCanvas, 0, 0);
+  pop();
+  //ellipse(width/2, height/2, 100, 100);
+}
+
+function celebrate() {
+  if (!this.celbrating && !this.celebrationSound.isPlaying()) {
+    this.celebrationSound.play();
+    this.celebrating = true;
+  }
+  image(confetti, 0, 0);
 }
